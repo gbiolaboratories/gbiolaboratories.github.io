3daba060f9a67d72ea2e8951015b9c9fdb662558
gbiolaboratories <77200639+gbiolaboratories@users.noreply.github.com>
2021-05-10 23:31:43 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@
 	 * -------------------------------------------------- */
 	var ssPreloader = function() {
 
-		$(document).ready(function() {
+		$WIN.on('load', function() {
 
 			// force page scroll position to top at page refresh
 			$('html, body').animate({ scrollTop: 0 }, 'normal');
